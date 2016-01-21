a87b20b5021cb5f158c48796a794a11624c4d4f1
zoejf <zoejfoss@gmail.com>
2016-01-21 15:09:07 -0800

    user can create new author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,6 +10,32 @@ class AuthorsController < ApplicationController
 		render :show
 	end
 
+	def new
+		@author = Author.new
+		render :new
+	end
+
+	def create	
+		author = Author.new(author_params)
+		if author.save
+			redirect_to author_path(author)
+		else
+			redirect_to new_author_path
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
 	private
 		def author_params
 			params.require(:author).permit(:first_name, :last_name, :year_born, :year_died, :image)
