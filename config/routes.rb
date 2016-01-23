620462447910848da8d777a22b638e3b91d09e5a
zoejf <zoejfoss@gmail.com>
2016-01-23 00:38:18 -0800

    users can sign up, log in, log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
 Rails.application.routes.draw do
- 
+
  root 'authors#index'
 
- get '/authors/:id/books', to: 'books#new', as: 'new_book'
-
- resources :books, only: [:index, :create, :show, :destroy]
+ #authors
  resources :authors
 
- resources :users, only: [:new, :create]
+ #books
+ resources :books, only: [:index, :create, :show, :destroy]
+ get '/authors/:id/books', to: 'books#new', as: 'new_book'
+
+ #users
+ get '/signup', to: 'users#new', as: 'signup'
+ resources :users, only: [:create]
+
+ #sessions
+ get '/login', to: 'sessions#new', as: 'login'
+ post '/login', to: 'sessions#create'
+ get '/logout', to: 'sessions#destroy'
+
 end
 
 
 #      Prefix Verb   URI Pattern                  Controller#Action
 #        root GET    /                            authors#index
-#    new_book GET    /authors/:id/books(.:format) books#new
-#       books GET    /books(.:format)             books#index
-#             POST   /books(.:format)             books#create
-#        book GET    /books/:id(.:format)         books#show
-#             DELETE /books/:id(.:format)         books#destroy
 #     authors GET    /authors(.:format)           authors#index
 #             POST   /authors(.:format)           authors#create
 #  new_author GET    /authors/new(.:format)       authors#new
@@ -26,5 +31,13 @@ end
 #             PATCH  /authors/:id(.:format)       authors#update
 #             PUT    /authors/:id(.:format)       authors#update
 #             DELETE /authors/:id(.:format)       authors#destroy
+#       books GET    /books(.:format)             books#index
+#             POST   /books(.:format)             books#create
+#        book GET    /books/:id(.:format)         books#show
+#             DELETE /books/:id(.:format)         books#destroy
+#    new_book GET    /authors/:id/books(.:format) books#new
+#      signup GET    /signup(.:format)            users#new
 #       users POST   /users(.:format)             users#create
-#    new_user GET    /users/new(.:format)         users#new
+#       login GET    /login(.:format)             sessions#new
+#             POST   /login(.:format)             sessions#create
+#      logout GET    /logout(.:format)            sessions#destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123062005) do
+ActiveRecord::Schema.define(version: 20160123072448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 20160123062005) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.text     "synopsis"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
